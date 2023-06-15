dc389e1496c8a762359705a7392fef65e8508517
Icecubesaad <saad.qad418@gmail.com>
2023-06-16 02:43:38 +0500

    project

---
+++ Sample programs/hekk_nah.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+    char array1[100] = {"Tomato  120 k   42"};
+    char array2[100] = {"Saad"};
+    cout<<array1[16];
+}
--- project/prever.cpp
+++ project/prever.cpp
@@ -1,77 +1,34 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-int* price_int = new int;
-void sorting_file(char array[], int minus, int size) {
-    cout << "REACHED SORTING FILE\n";
-    int temp = minus;
-    ifstream reading("Inventory.txt");
-    if (!reading.is_open()) {
-        cout << "Error opening the file\n";
-    }
-    else {
-        ofstream writing("Inventory2.txt");
-        if (!writing.is_open()) {
-            cout << "Some error occurred\n";
-        }
-        else {
-            char Data_from_file[100];
-            bool found = false;
-            while (reading.getline(Data_from_file, 100)) {
-                minus = temp;
-                found = false;
-                cout << "Total records: " << Data_from_file << endl;
-                while (array[minus] == Data_from_file[minus]) {
-                    if (minus == size - 1) {
-                        found = true;
-                        break;
-                    }
-                    minus++;
-                }
-                if (!found) {
-                    writing << Data_from_file << endl;
-                }
-            }
-            writing.close();
-            reading.close();
-        }
-    }
-}
-
-
-
-int Decrementing_quantity(int quantity,char name[],char unit,int size,int Minus,char array[]){
-
-    cout<<"REACHED DECREMENTING QUANTITY\n";
+int Decrementing_quantity(int quantity,char array[],int size,int Minus){
+    cout<<"Decrementing Quantity from array . \n";
+    cout<<quantity-1<<endl;
     fstream appending_file;
     appending_file.open("Inventory.txt",ios::app);
     if(!appending_file.is_open()){
         cout<<"Some error occured.";
     }
     else{
-        appending_file<<"\n"<<name<<"\t"<<*(price_int)<<"\t"<<unit<<"\t"<<quantity-1;
+        cout<<"Old array : "<<array<<endl;
+        array[Minus-1] = quantity-1;
+        cout<<"New array : "<<array<<endl;
     }
-    appending_file.close();
-    sorting_file(array,Minus,size);
 }
 
 int Converting_char_to_int(char price[], char quantity[])
 {
-    cout<<"REACHED CONVERSION\n";
-    int quantity_int = 0;
-    *(price_int) = 0;
-    int i = 0;
+    int quantity_int = 0,price_int = 0, i = 0;
     while (quantity[i] != '\0')
     {
 
         quantity_int = quantity_int * 10 + (quantity[i] - '0');
         i++;
     }
-    i=0;
     while (price[i] != '\0')
     {
 
-        *(price_int) = *(price_int) * 10 + (price[i] - '0');
+        price_int = price_int * 10 + (quantity[i] - '0');
         i++;
     }
    return quantity_int;
@@ -87,23 +44,19 @@ int size_of_array(char array[])
 }
 char Finding_parameters(char array[])
 {
-    cout<<"REACHED Finding_parameters\n";
     int size,Minus_from_size;
     size = size_of_array(array);
+    cout << "\nSize from the function : " << size << endl;
     char price[100] = {};
     char unit;
-    char name[100]={};
     char quantity[100] = {};
     int i = 0, j = 0;
     while (array[i] != '\0')
     {
         while (array[i] != ' ')
         {
-            name[j] = array[i];
             i++;
-            j++;
         }
-        j=0;
         while (array[i] == ' ')
         {
             i++;
@@ -144,14 +97,16 @@ char Finding_parameters(char array[])
             }
         }
         quantity[j] = '\0';
+        cout << "Quanitiy : " << quantity << endl;
+        cout << "Price : " << price << endl;
+        cout << "Unit : " << unit << endl;
         int quantity_int;
         quantity_int =  Converting_char_to_int(price, quantity);
-        Decrementing_quantity(quantity_int,name,unit,size,Minus_from_size,array);
+        Decrementing_quantity(quantity_int,array,size,Minus_from_size);
     }
 }
 char finding_specific_data(char array[], int size)
 {
-    cout<<"REACHED finding_specific_data \n";
     bool found = false;
     fstream file;
     int i = 0, j = 0;
@@ -170,6 +125,8 @@ char finding_specific_data(char array[], int size)
             {
                 if (i == size - 1)
                 {
+                    cout << "Found the specific data : \n";
+                    cout << data_from_file;
                     found = true;
                     Finding_parameters(data_from_file);
                     break;
@@ -182,7 +139,6 @@ char finding_specific_data(char array[], int size)
             cout << "Searched Item doesnt exist.\n";
         }
     }
-    file.close();
 }
 
 int Inventory()
@@ -199,5 +155,4 @@ int Inventory()
 int main()
 {
     Inventory();
-    delete price_int;
 }
